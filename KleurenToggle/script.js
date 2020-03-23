824ffb0e4e173db3b2e0a213d14113ab553ffb2d
Marcel van der Meer <marcelmeer@gmail.com>
2020-03-23 11:23:09 +0100

    Cleared some commented rules

--- KleurenToggle/script.js
+++ KleurenToggle/script.js
@@ -83,11 +83,6 @@ document.addEventListener("keydown", function (event) {
     if (event.keyCode === 49 || event.keyCode === 97) {
         document.getElementById("kleur").innerHTML = 'grijs';
         body.style.background = 'gray';
-        /* body.classList.toggle('redBackground', false);
-         body.classList.toggle('orangeBackground', false);
-         body.classList.toggle('purpleBackground', false);
-         body.classList.toggle('greenBackground', false);
-         body.classList.toggle('greyBackground', true); */
         menu.classList.toggle('visible', false);
         menu.classList.toggle('hidden', true);
     }
@@ -95,11 +90,6 @@ document.addEventListener("keydown", function (event) {
     else if (event.keyCode === 50 || event.keyCode === 98) {
         document.getElementById("kleur").innerHTML = 'rood';
         body.style.background = 'red';
-        /* body.classList.toggle('greyBackground', false);
-         body.classList.toggle('orangeBackground', false);
-         body.classList.toggle('purpleBackground', false);
-         body.classList.toggle('greenBackground', false);
-         body.classList.toggle('redBackground', true); */
         menu.classList.toggle('visible', false);
         menu.classList.toggle('hidden', true);
     }
@@ -107,11 +97,6 @@ document.addEventListener("keydown", function (event) {
     else if (event.keyCode === 51 || event.keyCode === 99) {
         document.getElementById("kleur").innerHTML = 'oranje';
         body.style.background = 'orange';
-        /* body.classList.toggle('greyBackground', false);
-         body.classList.toggle('redBackground', false);
-         body.classList.toggle('purpleBackground', false);
-         body.classList.toggle('greenBackground', false);
-         body.classList.toggle('orangeBackground', true); */
         menu.classList.toggle('visible', false);
         menu.classList.toggle('hidden', true);
     }
@@ -119,11 +104,6 @@ document.addEventListener("keydown", function (event) {
     else if (event.keyCode === 52 || event.keyCode === 100) {
         document.getElementById("kleur").innerHTML = 'paars';
         body.style.background = 'purple';
-        /* body.classList.toggle('greyBackground', false);
-        body.classList.toggle('redBackground', false);
-        body.classList.toggle('orangeBackground', false);
-        body.classList.toggle('greenBackground', false);
-        body.classList.toggle('purpleBackground', true); */
         menu.classList.toggle('visible', false);
         menu.classList.toggle('hidden', true);
     }
